8cb5fcf5062f87300a74f127494e6d9974914f66
sadwave <99206632+sadwavecodes@users.noreply.github.com>
2023-06-15 13:49:53 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").onclick = function () {
+    location.href = "weapons.html";
+    };
+
+    let text = document.lastModified;
+    document.getElementById("modification").innerHTML = text;
+    
